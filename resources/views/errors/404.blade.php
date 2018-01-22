65bf146e2256721d416300917d311c90a97091fe
GreenWhite <greenwhitedev@gmail.com>
2018-01-22 09:39:28 +0000

    Starting off stuff with admin

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/21/18
+ * Time: 12:28 AM
+ */
---
+++ resources/views/layouts/error.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/21/18
+ * Time: 12:14 AM
+ */
