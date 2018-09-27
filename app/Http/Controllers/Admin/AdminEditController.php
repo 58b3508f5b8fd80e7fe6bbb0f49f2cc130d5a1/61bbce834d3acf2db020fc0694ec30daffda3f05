0c1fdb1db8e36d9d13602e590a007a2cad08e32d
GreenWhite <greenwhitedev@gmail.com>
2018-09-27 09:42:58 +0100

    pagination for Json requests

--- app/Http/Controllers/Admin/AdminEditController.php
+++ app/Http/Controllers/Admin/AdminEditController.php
@@ -20,12 +20,10 @@ class AdminEditController extends Controller
         $for = $request->input('for');
         $id = $request->input('id');
         $admin = User::find($id - 1427);
-
         $admin->first_name = $request->input('first_name');
         $admin->last_name = $request->input('last_name');
         $admin->email = $request->input('email');
         $admin->access_level = $request->input('access_level');
-
         if ($admin->save()) {
             $message = "New Admin was edited successfully";
         } else {
@@ -55,7 +53,6 @@ class AdminEditController extends Controller
             $userMeta->form_location = $request->file('form_location')
                 ->store('tlssavings/app/images/forms');
         }
-
         if ($request->hasFile('signature_location')
             && $request->file('signature_location')->isValid()
         ) {
@@ -63,7 +60,6 @@ class AdminEditController extends Controller
             $userMeta->signature_location = $request->file('signature_location')
                 ->store('tlssavings/app/images/signatures');
         }
-
         if ($request->hasFile('utility_bill_location')
             && $request->file('utility_bill_location')->isValid()
         ) {
@@ -72,7 +68,6 @@ class AdminEditController extends Controller
                 = $request->file('utility_bill_location')
                 ->store('tlssavings/app/images/utility_bills');
         }
-
         if ($request->hasFile('idcard_location')
             && $request->file('idcard_location')->isValid()
         ) {
@@ -80,7 +75,6 @@ class AdminEditController extends Controller
             $userMeta->idcard_location = $request->file('idcard_location')
                 ->store('tlssavings/app/images/idcards');
         }
-
         if ($request->hasFile('passport_location')
             && $request->file('passport_location')->isValid()
         ) {
@@ -89,9 +83,7 @@ class AdminEditController extends Controller
                 ->store('tlssavings/app/images/passport');
             $userMeta->passport_location = $passport;
         }
-
         array_push($details, ['updated_at' => date('Y-m-d H:i:s')]);
-
         unset($details['signature_location']);
         unset($details['form_location']);
         unset($details['utility_bill_location']);
@@ -102,9 +94,7 @@ class AdminEditController extends Controller
         unset($details['grade']);
         unset($details['for']);
         unset($details[0]);
-
         $isUpdated = User_meta::where('id', $id)->update($details);
-
         $userTable = User::where('wallet_address',
             $userMeta['wallet_address'])
             ->update([
@@ -122,7 +112,6 @@ class AdminEditController extends Controller
         }
         $data['action'] = 'new user';
         return $this->getUsers('user', $for, $message);
-
     }
 
     public function getUsers($type, $for, $message)
@@ -131,49 +120,50 @@ class AdminEditController extends Controller
             case 'active':
                 $data['users'] = User::where('type', 'user')
                     ->where('status', 'active')
-                    ->get();
+                    ->orderBy('created_at', 'desc')->paginate(800);
                 break;
             case 'admin':
                 $data['users'] = User::where('type', 'admin')
                     ->where('access_level', ' < ',
                         Auth::user()->access_level)
-                    ->get();
+                    ->orderBy('created_at', 'desc')->paginate(800);
                 break;
             case 'all':
                 $data['users'] = User::where('type', 'user')
                     ->orderBy('status')
-                    ->orderBy('first_name')->get();
+                    ->orderBy('first_name')->orderBy('created_at', 'desc')
+                    ->paginate(800);
                 break;
             case 'blocked':
                 $data['users'] = User::where('type', 'user')
                     ->where('status', 'blocked')
-                    ->get();
+                    ->orderBy('created_at', 'desc')->paginate(800);
                 break;
             case 'registered':
                 $data['users'] = User_meta::where('status', 'registered')
-                    ->get();
+                    ->orderBy('created_at', 'desc')->paginate(800);
                 break;
             case 'unregistered':
                 $data['users'] = User_meta::where('status', 'unregistered')
-                    ->orWhere('status', 'pending')->get();
+                    ->orWhere('status', 'pending')
+                    ->orderBy('created_at', 'desc')->paginate(800);
                 break;
             case 'suspended':
                 $data['users'] = User::where('type', 'user')
                     ->where('status', 'pending')
-                    ->get();
+                    ->orderBy('created_at', 'desc')->paginate(800);
                 break;
             default:
                 $data['users'] = User::where('type', 'user')
                     ->orderBy('status')
-                    ->orderBy('first_name')->get();
+                    ->orderBy('first_name')->orderBy('created_at', 'desc')
+                    ->paginate(800);
                 break;
         }
-
         $data['action'] = $for;
         $data['type'] = $type;
         $html = View::make('admin.partials.user', $data);
         $html = $html->render();
-
         return response()->json([
             'status'  => 'success',
             'message' => $message,
@@ -185,41 +175,33 @@ class AdminEditController extends Controller
         Request $request
     ) {
         $id = $request->input('id');
-
         $data['admin'] = User::find($id - 9407);
-
         $html = View::make('admin.partials.editAdmin', $data);
         $html = $html->render();
-
         return response()->json([
             'status' => 'success',
             'html'   => $html
         ]);
-
     }
 
     public function getUser(Request $request)
     {
-        $id = (int) $request->input('id');
+        $id = $request->input('id');
         $action = $request->input('action');
         $data = null;
         if (!in_array($action, ['registered', 'unregistered'])) {
             $data['user'] = User::join('user_metas', 'users.wallet_address',
-                '=', 'user_metas.wallet_address')->where('users.id', $id - 1107)
+                '=', 'user_metas.wallet_address')->where('users.id', $id - 9407)
                 ->select('user_metas.*')->first();
         } else {
-            $data['user'] = User_meta::where('id', $id - 1107)->first();
+            $data['user'] = User_meta::where('id', $id - 9407)->first();
         }
-
         $html = View::make('admin.partials.editUser', $data);
         $html = $html->render();
-
-
         return response()->json([
             'status' => 'success',
             'html'   => $html
         ]);
-
     }
 
     public function linkAccount(Request $request)
@@ -230,14 +212,13 @@ class AdminEditController extends Controller
         $userMeta->bank_name = $request->bank_name;
         $userMeta->bank_acc_name = $request->acc_name;
         $userMeta->bank_acc_no = $request->acc_no;
-
         if ($userMeta->save()) {
             $sms = new SendSMS();
             $to = $userMeta->phone_no;
             $message = "Hello " . $userMeta->first_name
                 . ",\nYour account with " . config('app.name')
                 . " has been linked to the bank successfully.\nBank Name: $userMeta->bank_name,\nAcc. Name: $userMeta->bank_acc_name,\nAcc. No: $userMeta->bank_acc_no.\nPlease contact us for more enquiries.\n"
-                . config('app.name').".";
+                . config('app.name') . ".";
             $resp = $sms->sendSMS($to, $message);
             return response()->json([
                 'message' => 'Account has been linked successfully'
